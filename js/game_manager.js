a48bea4ee2d752ddb6a0fc63835e9bd053e59621
TheReal58 <68038765+TheReal58@users.noreply.github.com>
2020-07-09 14:02:25 -0700

    Changed the start tiles from 1 to 5.

--- js/game_manager.js
+++ js/game_manager.js
@@ -62,7 +62,7 @@ GameManager.prototype.addRandomTile = function () {
   if (this.grid.cellsAvailable()) {
     var value = 5; // always spawn a 5.
 	
-	this.score += 1;
+	this.score += 5;
 	
     var tile = new Tile(this.grid.randomAvailableCell(), value);
 
